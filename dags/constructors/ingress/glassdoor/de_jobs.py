253123521173ef2b9986c3d1b9e0ca86fe6076bc
Kevin Cao <kevincao222@gmail.com>
2026-02-14 22:39:05 -0800

    Test dag

---
+++ dags/constructors/ingress/glassdoor/de_jobs.py
@@ -0,0 +1,29 @@
+import asyncio
+from airflow.sdk import dag, task
+
+from apps import af_utils
+from apps.webscraper_utils import GlassdoorScraper
+
+
+@dag(
+    dag_id=af_utils.get_dag_name(__file__),
+    default_args=af_utils.get_default_args(),
+    schedule=None,
+    catchup=False
+)
+def dag():
+
+    @task
+    def run_scraper():
+        async def execute_scrape():
+            async with GlassdoorScraper() as scraper:
+                return await scraper.scrape_job_listings()
+        results = asyncio.run(execute_scrape())
+        
+        print(f"Scraped {len(results) if results else 0} jobs.")
+        return results
+
+    run_scraper()
+
+
+dag()
