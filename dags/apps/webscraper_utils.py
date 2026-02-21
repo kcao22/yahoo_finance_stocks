b9072b013371870d49527b1763dbae8d7188f359
kcao22 <kevincao222@gmail.com>
2026-02-21 14:17:08 -0800

    Modify webscraper utils to be re-useable for daily and weekly scrapes

--- dags/apps/webscraper_utils.py
+++ dags/apps/webscraper_utils.py
@@ -1,9 +1,9 @@
 import asyncio
-from concurrent.futures import ThreadPoolExecutor, as_completed
+import random
 
 from playwright.async_api import async_playwright
 from playwright_stealth import Stealth
-from apps.data_source_utils.yahoo_finance_config import DAILY_EXTRACT_CONFIG
+from apps.data_source_utils.yahoo_finance_config import DAILY_EXTRACT_CONFIG, DIM_DATA_EXTRACT_CONFIG
 
 class WebScraper:
     def __init__(self, websocket_endpoint: str = "ws://playwright-browser:3000/"):
@@ -30,7 +30,7 @@ class WebScraper:
         )        
         # Set user agent and viewport settings in new context session
         self.context = await self.browser.new_context(
-            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+            user_agent=self._get_random_user_agent(),
             viewport={"width": 1920, "height": 1080}
         )
         self.page = await self.context.new_page()
@@ -43,6 +43,18 @@ class WebScraper:
             await self.browser.close()
         if self.playwright:
             await self.playwright.stop()
+
+    def _get_random_user_agent(self) -> str:
+        user_agents = [
+            # Chrome (Windows)
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            # Firefox (Windows)
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0',
+            # Edge (Windows)
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0',
+        ]
+        return random.choice(user_agents)
 
     async def click_button(self, page, button_selector, selector_desc: str, state: str = "visible", timeout: int = 10000):
         """
@@ -81,44 +93,56 @@ class YahooFinanceScraper(WebScraper):
         super().__init__(websocket_endpoint)
         self.base_url = "https://finance.yahoo.com/quote"
 
-    async def scrape_company_stock_data(self, company_symbol: str) -> list[dict]:
+    async def scrape_company_data(self, company_symbol: str, extract_config: list[dict]) -> list[dict]:
         """
         Scrapes a single company's stock data from Yahoo Finance given a company symbol (e.g. AAPL for Apple, MSFT for Microsoft).
         Iterates over DAILY_EXTRACT_CONFIG to extract relevant stock data points based on provided CSS selectors and returns a dictionary of extracted data.
-        :param company_symbol: Stock ticker symbol for company of interest
+        :param company_symbol: Stock ticker symbol for company of interest.
+        :param extract_config: The config list of dictionaries outlining target field and corresponding CSS selector for extraction.
         :return: Dictionary of extracted stock data for given company
         """
         # Launch with context to use specific user agent settings / viewport settings
         # Browser is also heavier / more resource intensive
         page = await self.context.new_page()
         # Proceed once basic HTML loads
-        self.url = f"{self.base_url}/{company_symbol}"
-        print(f"Navigating to {self.url}...")
-        await page.goto(self.url, wait_until="domcontentloaded", timeout=60000)
-        company_stock_data = {}
-        for extract_mappings in DAILY_EXTRACT_CONFIG:
-            # Extract stock data from page
+        extract_url = f"{self.base_url}/{company_symbol}" if extract_config == DAILY_EXTRACT_CONFIG else f"{self.base_url}/{company_symbol}/profile"
+        print(f"Navigating to {extract_url}...")
+        await page.goto(extract_url, wait_until="domcontentloaded", timeout=60000)
+        if extract_url.endswith("/profile"):
+            try:
+                await page.wait_for_selector("a[href*='/sectors/']", timeout=5000)
+            except:
+                print(f"Timed out waiting for profile links for {company_symbol}")
+        data = {}
+        for extract_mappings in extract_config:
+            # Extract data from page
             data_value = await self.locate_text(
                 page=page,
                 locator_class=extract_mappings["selector_field"],
                 locator_desc=extract_mappings["locator_desc"]
-                
             )
-            print(f"{extract_mappings['target_field']}: {data_value}")
-            company_stock_data[extract_mappings["target_field"]] = data_value
-        return company_stock_data
+            data[extract_mappings["target_field"]] = data_value
+        return data
 
-    async def scrape_companies_data(self, company_symbols: list[str], max_concurrency: int = 10) -> list[dict]:
+    async def scrape_companies_data(self, company_symbols: list[str], stock_or_profile: str, max_concurrency: int = 10) -> list[dict]:
         """
-        Scrapes stock data for multiple companies concurrently from Yahoo Finance.
+        Scrapes companies data for multiple companies concurrently from Yahoo Finance.
         :param company_symbols: List of stock ticker symbols for companies of interest
+        :param stock_or_profile: Whether to extract stock data or profile data for the companies. Determines which config to use for extraction.
         :param max_concurrency: Maximum number of concurrent scraping tasks
         :return: List of dictionaries containing extracted stock data for each company
         """
+        if stock_or_profile not in ["stock", "profile"]:
+            raise ValueError(f"Invalid stock_or_profile value: {stock_or_profile}. Must be 'stock' or 'profile'.")
+        if stock_or_profile == "stock":
+            extract_config = DAILY_EXTRACT_CONFIG
+        else:
+            extract_config = DIM_DATA_EXTRACT_CONFIG
         semaphore = asyncio.Semaphore(max_concurrency)
 
         async def sem_task(symbol):
             async with semaphore:
-                return await self.scrape_company_stock_data(symbol)
+                return await self.scrape_company_data(company_symbol=symbol, extract_config=extract_config)
+
         tasks = [sem_task(symbol) for symbol in company_symbols]
         return await asyncio.gather(*tasks)
